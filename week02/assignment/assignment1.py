de50b60bf13a35051d29abc6ecad06c3efffbd1c
ZHUANGDONGRUN <xanxusdong@163.com>
2022-02-02 15:30:35 +0800

    Update assignment1.py
    HW1

--- week02/assignment/assignment1.py
+++ week02/assignment/assignment1.py
@@ -7,7 +7,10 @@
 
 
 def factorial(x: int) -> int:
-    return 1
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x * factorial(x - 1)
 
 
 assert factorial(0) == 1
@@ -19,7 +22,10 @@ assert factorial(9) == 362880
 # [1 + 2 + ... + x] and x is always >= 1.
 
 def print_sum(x: int) -> str:
-    return ""
+    result = 0
+    for i in range(1, x + 1):
+        result = result + i
+    return str(result)
 
 
 assert print_sum(1) == "1"
@@ -30,7 +36,12 @@ assert print_sum(5) == "15"
 # Q3. Write a program to check is a year is leap year (x is always > 0)
 
 def is_leap_year(year: int) -> bool:
-    return False
+    if year % 400 == 0:
+        return True
+    elif year % 4 == 0 and year % 100 != 0:
+        return True
+    else:
+        return False
 
 
 assert is_leap_year(2000)
@@ -42,7 +53,10 @@ assert not is_leap_year(2001)
 # Q4. Write a program to convert a list of lowercase words to uppercase words.
 
 def to_upper_case(words: [str]) -> [str]:
-    return []
+    return [str(words).upper()]
+
+words = ["abc", "de"]
+a = [str(words).upper()]
 
 
 assert to_upper_case(["abc", "de"]) == ["ABC", "DE"]
@@ -53,7 +67,12 @@ assert to_upper_case(["Amazon", "Apple"]) == ["AMAZON", "APPLE"]
 # https://baike.baidu.com/item/%E5%BC%82%E6%88%96/10993677?fromtitle=xor&fromid=64178
 
 def xor(a: bool, b: bool) -> bool:
-    return False
+    if a and b is True:
+        return False
+    elif a or b is True:
+        return True
+    else:
+        return False
 
 
 assert not xor(True, True)
@@ -65,12 +84,28 @@ assert not xor(False, False)
 # Q6. Write a Python program to display the current date and time under standard ISO 8601. e.g. 2021-12-03T10:15:30Z
 
 def get_current_time() -> str:
-    return ""
+    import datetime
+    return datetime.datetime.now().replace(microsecond=0).isoformat() + "Z"
 
+import datetime
+a = datetime.datetime.now().replace(microsecond=0).isoformat()
 
 assert "T" in get_current_time()
 assert "Z" in get_current_time()
 assert 20 == len(get_current_time())
 
+
 # Q7. Write a Python program to sum of two given integers. However, if the sum is between 15 to 20 it will return 20
 # please define function and test yourself.
+
+def zdr(a: int, b: int) -> int:
+    if 15 <= a + b < 20:
+        return 20
+    else:
+        return a + b
+
+
+a = int(input("请输入a："))
+b = int(input("请输入b："))
+result = zdr(a, b)
+print(result)
